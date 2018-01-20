bdb95029477ccd0c49ee72ae12cfb00dccb39d7d
Chris Chasm <chris@chasm.solutions>
2018-01-20 11:35:05 -0700

    Initialize file structure to project

---
+++ disciple-tools-facebook.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Plugin Name: Disciple Tools - Facebook - Integration
+ * Plugin URI: https://github.com/ZumeProject/disciple-tools-facebook
+ * Description: Disciple Tools - Facebook - Integration extends the Disciple Tools system to send and receive remote submissions from webform contacts.
+ * Version:  0.1.0
+ * Author URI: https://github.com/DiscipleTools
+ * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-facebook
+ * Requires at least: 4.7.0
+ * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
+ * Tested up to: 4.9
+ *
+ * @package Disciple_Tools
+ * @link    https://github.com/DiscipleTools
+ * @license GPL-2.0 or later
+ *          https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Singleton class for setting up the plugin.
+ *
+ * @since  0.1
+ * @access public
+ */
+class DT_Facebook {
+
+    /**
+     * Declares public variables
+     *
+     * @since  0.1
+     * @access public
+     * @return object
+     */
+    public $token;
+    public $version;
+    public $dir_path = '';
+    public $dir_uri = '';
+    public $img_uri = '';
+    public $includes_path;
+
+    /**
+     * Returns the instance.
+     *
+     * @since  0.1
+     * @access public
+     * @return object
+     */
+    public static function get_instance() {
+
+        static $instance = null;
+
+        if ( is_null( $instance ) ) {
+            $instance = new dt_facebook();
+            $instance->setup();
+            $instance->includes();
+            $instance->setup_actions();
+        }
+        return $instance;
+    }
+
+    /**
+     * Constructor method.
+     *
+     * @since  0.1
+     * @access private
+     * @return void
+     */
+    private function __construct() {
+    }
+
+    /**
+     * Loads files needed by the plugin.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    private function includes() {
+
+    }
+
+    /**
+     * Sets up globals.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    private function setup() {
+
+        // Main plugin directory path and URI.
+        $this->dir_path     = trailingslashit( plugin_dir_path( __FILE__ ) );
+        $this->dir_uri      = trailingslashit( plugin_dir_url( __FILE__ ) );
+
+        // Plugin directory paths.
+        $this->includes_path      = trailingslashit( $this->dir_path . 'includes' );
+
+        // Plugin directory URIs.
+        $this->img_uri      = trailingslashit( $this->dir_uri . 'img' );
+
+        // Admin and settings variables
+        $this->token             = 'dt_facebook';
+        $this->version             = '0.1';
+    }
+
+    /**
+     * Sets up main plugin actions and filters.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    private function setup_actions() {
+
+        // Internationalize the text strings used.
+        add_action( 'plugins_loaded', array( $this, 'i18n' ), 2 );
+
+        // Register activation hook.
+        register_activation_hook( __FILE__, array( $this, 'activation' ) );
+    }
+
+    /**
+     * Method that runs only when the plugin is activated.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    public function activation() {
+
+    }
+
+    /**
+     * Loads the translation files.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    public function i18n() {
+        load_plugin_textdomain( 'dt_facebook', false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ). 'languages' );
+    }
+
+    /**
+     * Magic method to output a string if trying to use the object as a string.
+     *
+     * @since  0.1
+     * @access public
+     * @return string
+     */
+    public function __toString() {
+        return 'dt_facebook';
+    }
+
+    /**
+     * Magic method to keep the object from being cloned.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    public function __clone() {
+        _doing_it_wrong( __FUNCTION__, esc_html__( 'Whoah, partner!', 'dt_facebook' ), '0.1' );
+    }
+
+    /**
+     * Magic method to keep the object from being unserialized.
+     *
+     * @since  0.1
+     * @access public
+     * @return void
+     */
+    public function __wakeup() {
+        _doing_it_wrong( __FUNCTION__, esc_html__( 'Whoah, partner!', 'dt_facebook' ), '0.1' );
+    }
+
+    /**
+     * Magic method to prevent a fatal error when calling a method that doesn't exist.
+     *
+     * @since  0.1
+     * @access public
+     * @return null
+     */
+    public function __call( $method = '', $args = array() ) {
+        // @codingStandardsIgnoreLine
+        _doing_it_wrong( "dt_facebook::{$method}", esc_html__( 'Method does not exist.', 'dt_facebook' ), '0.1' );
+        unset( $method, $args );
+        return null;
+    }
+}
+
+/**
+ * Gets the instance of the `dt_facebook` class.  This function is useful for quickly grabbing data
+ * used throughout the plugin.
+ *
+ * @since  0.1
+ * @access public
+ * @return object
+ */
+function dt_facebook() {
+    return DT_Facebook::get_instance();
+}
+
+// Let's roll!
+add_action( 'plugins_loaded', 'dt_facebook' );
