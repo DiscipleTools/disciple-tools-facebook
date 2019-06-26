1368d47416b1be9d6b7e477cccdca835678863c1
corsacca <corsacca@gmail.com>
2019-06-26 16:49:27 +0100

    phpcs

--- includes/dt-hooks.php
+++ includes/dt-hooks.php
@@ -7,10 +7,10 @@ if ( !defined( 'ABSPATH' ) ) {
  * Hooks that are always available on any request.
  */
 
-add_filter( "dt_search_extra_post_meta_fields", "add_fields_in_dt_search" );
+add_filter( "dt_search_extra_post_meta_fields", "dt_add_fields_in_dt_search" );
 
 
-function add_fields_in_dt_search( $fields ){
+function dt_add_fields_in_dt_search( $fields ){
     $fields[] = "facebook_data";
     return $fields;
 }
