39d3160dc6b9aca705e4b080ea29965e27b616e4
Chris Chasm <chris@chasm.solutions>
2020-02-04 10:08:30 -0700

    Multisite: Added filter for admin warning if in multisite with mixed themes. This change mutes warning notice in non-dt themes, while allowing it to be network enabled or must-use.

--- disciple-tools-facebook.php
+++ disciple-tools-facebook.php
@@ -26,6 +26,7 @@ $dt_facebook_required_dt_theme_version = '0.21.0';
  *
  * @since  0.1
  * @access public
+ * @return object|bool
  */
 function dt_facebook() {
     global $dt_facebook_required_dt_theme_version;
@@ -36,9 +37,12 @@ function dt_facebook() {
      */
     $is_theme_dt = strpos( $wp_theme->get_template(), "disciple-tools-theme" ) !== false || $wp_theme->name === "Disciple Tools";
     if ( !$is_theme_dt || version_compare( $version, $dt_facebook_required_dt_theme_version, "<" ) ) {
-        add_action( 'admin_notices', 'dt_facebook_hook_admin_notice' );
-        add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
-        return new WP_Error( 'current_theme_not_dt', 'Disciple Tools Theme not active or not the latest version.' );
+        if ( ! is_multisite() ) {
+            add_action('admin_notices', 'dt_facebook_hook_admin_notice');
+            add_action('wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler');
+        }
+        
+        return false;
     }
     /**
      * Load useful function from the theme
