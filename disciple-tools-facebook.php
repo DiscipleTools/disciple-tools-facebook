618f8dcaf0bd8c08e2c2a9075aeed9c30d2a0e7f
corsacca <corsacca@gmail.com>
2020-02-25 15:31:05 +0100

    phpcs

--- disciple-tools-facebook.php
+++ disciple-tools-facebook.php
@@ -38,10 +38,9 @@ function dt_facebook() {
     $is_theme_dt = strpos( $wp_theme->get_template(), "disciple-tools-theme" ) !== false || $wp_theme->name === "Disciple Tools";
     if ( !$is_theme_dt || version_compare( $version, $dt_facebook_required_dt_theme_version, "<" ) ) {
         if ( ! is_multisite() ) {
-            add_action('admin_notices', 'dt_facebook_hook_admin_notice');
-            add_action('wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler');
+            add_action( 'admin_notices', 'dt_facebook_hook_admin_notice' );
+            add_action( 'wp_ajax_dismissed_notice_handler', 'dt_hook_ajax_notice_handler' );
         }
-        
         return false;
     }
     /**
