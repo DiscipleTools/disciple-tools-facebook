a42adfb1560ee0b4cc1f7519734e7465fe44a9aa
corsacca <corsacca@gmail.com>
2021-02-08 17:42:38 +0100

    Remove duplicate menu declaration: Extensions (DT)

--- includes/admin/admin-menu-and-tabs.php
+++ includes/admin/admin-menu-and-tabs.php
@@ -55,10 +55,6 @@ class DT_Facebook_Menu {
      * @since 0.1
      */
     public function register_menu() {
-        add_menu_page( __( 'Extensions (DT)', 'disciple_tools' ), __( 'Extensions (DT)', 'disciple_tools' ), 'manage_dt', 'dt_extensions', [
-            $this,
-            'extensions_menu'
-        ], 'dashicons-admin-generic', 59 );
         add_submenu_page( 'dt_extensions', __( 'Facebook', 'dt_facebook' ), __( 'Facebook', 'dt_facebook' ), 'manage_dt', $this->token, [
             $this,
             'content'
